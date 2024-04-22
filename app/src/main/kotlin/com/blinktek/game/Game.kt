3306614265e24022e5c226a978a92e1355997d60
Jareem Hoff <jareem.e.hoff@gmail.com>
2024-04-21 22:34:55 -0400

    finished extracting vk instance creation into it's own class

--- app/src/main/kotlin/com/blinktek/game/Game.kt
+++ app/src/main/kotlin/com/blinktek/game/Game.kt
@@ -1,21 +1,18 @@
 package com.blinktek.game
 
 import com.blinktek.display.Window
-import java.nio.Buffer
-import java.nio.ByteBuffer
 import kotlin.io.println
 import org.lwjgl.glfw.GLFW.*
 import org.lwjgl.glfw.GLFWVulkan.*
-import org.lwjgl.system.MemoryStack
 import org.lwjgl.system.MemoryUtil.*
 import org.lwjgl.vulkan.*
-import org.lwjgl.vulkan.KHRSurface.VK_KHR_SURFACE_EXTENSION_NAME
+import org.lwjgl.vulkan.VK10.*
 import org.lwjgl.vulkan.VK13.*
 
 class Game {
 
     var window: Window = Window()
-    var vk_instance: Long? = null
+    var vk_instance: VkManager = VkManager()
 
     constructor() {
         println("Game Class Created")
@@ -30,8 +27,8 @@ class Game {
     }
 
     fun init() {
+        vk_instance.init()
         window.init()
-        init_vulkan()
     }
 
     fun loop() {
@@ -47,6 +44,7 @@ class Game {
     fun render() {}
 
     fun cleanup() {
+        vk_instance.cleanup()
         window.cleanup()
         glfwTerminate()
     }
@@ -56,80 +54,4 @@ class Game {
     fun keyboard_input() {}
 
     fun mouse_input() {}
-
-    fun init_vulkan() {
-        vk_instance = vk_create_instance()
-    }
-
-    fun vk_create_instance(): Long {
-        MemoryStack.stackPush().use { stack ->
-            // Check for supported extensions:
-            val supportedExtensions = vk_list_supported_extensions()
-            println("Supported Vulkan Extensions:")
-            supportedExtensions.forEach { println("\t- " + it) }
-
-            // Application info setup:
-            val appInfo: VkApplicationInfo = VkApplicationInfo.calloc(stack)
-            appInfo.sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
-            appInfo.pApplicationName(stack.UTF8("Hello Triangle"))
-            appInfo.applicationVersion(VK_MAKE_VERSION(1, 0, 0))
-            appInfo.pEngineName(stack.UTF8("No Engine"))
-            appInfo.engineVersion(VK_MAKE_VERSION(1, 0, 0))
-            appInfo.apiVersion(VK_API_VERSION_1_3)
-            println("application information:\n" + appInfo)
-            
-            // Instance create info setup:
-            val createInfo = VkInstanceCreateInfo.calloc(stack)
-            createInfo.sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
-            createInfo.pApplicationInfo(appInfo)
-            
-            // Specify global extensions and validation layers:
-            val extensionName = stack.UTF8Safe(VK_KHR_SURFACE_EXTENSION_NAME)
-            if (extensionName == null) {
-                throw RuntimeException("Required extens name is null")
-            }
-            val extensions = stack.pointers(extensionName as Buffer)
-            createInfo.ppEnabledExtensionNames(extensions)
-            
-            // Create the vulkan instance:
-            val pInstance = stack.mallocPointer(1)
-            val err = vkCreateInstance(createInfo, null, pInstance)
-            if (err != VK_SUCCESS) {
-                throw RuntimeException("Failed to create Vulkan instance: error code $err")
-            }
-            println("Instance information:\n" + createInfo)
-            
-            // Return the created instance:
-            return pInstance.get(0)
-        }
-    }
-
-    fun vk_list_supported_extensions(): List<String> {
-        MemoryStack.stackPush().use { stack ->
-            // Allocate an int buffer to hold the extension count:
-            val ip = stack.mallocInt(1)
-
-            // First call to get the number of extensions:
-            var err = vkEnumerateInstanceExtensionProperties(null as ByteBuffer?, ip, null)
-            if (err != VK_SUCCESS) {
-                throw RuntimeException(
-                        "Failed to get the number instance extensions: err code\n $err"
-                )
-            }
-
-            // Allocate a buffer to hold the extension properties:
-            val extensions = VkExtensionProperties.malloc(ip.get(0), stack)
-
-            // Second call to get the extension properties:
-            err = vkEnumerateInstanceExtensionProperties(null as ByteBuffer?, ip, extensions)
-            if (err != VK_SUCCESS) {
-                throw RuntimeException(
-                        "Failed to enumeratea Vulkan instance extensions: err code\n $err"
-                )
-            }
-
-            // Extract the extension names and return them as a list:
-            return extensions.map { it.extensionNameString() }
-        }
-    }
 }
---
+++ app/src/main/kotlin/com/blinktek/game/VulkanInstance.kt
@@ -0,0 +1,97 @@
+package com.blinktek.game
+
+import java.nio.*
+import org.lwjgl.system.*
+import org.lwjgl.vulkan.*
+import org.lwjgl.vulkan.KHRSurface.*
+import org.lwjgl.vulkan.VK10.*
+import org.lwjgl.vulkan.VK13.*
+
+class VkManager {
+    var vk_instance: VkInstance? = null
+        private set
+
+    // Creates the instance for vulkan:
+    fun init() {
+        MemoryStack.stackPush().use { stack ->
+
+            // Application info setup:
+            val appInfo: VkApplicationInfo = VkApplicationInfo.calloc(stack)
+            appInfo.sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
+            appInfo.pApplicationName(stack.UTF8("Hello Triangle"))
+            appInfo.applicationVersion(VK_MAKE_VERSION(1, 0, 0))
+            appInfo.pEngineName(stack.UTF8("No Engine"))
+            appInfo.engineVersion(VK_MAKE_VERSION(1, 0, 0))
+            appInfo.apiVersion(VK_API_VERSION_1_3)
+            println("application information:\n" + appInfo)
+
+            // Instance create info setup:
+            val createInfo: VkInstanceCreateInfo = VkInstanceCreateInfo.calloc(stack)
+            createInfo.sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
+            createInfo.pApplicationInfo(appInfo)
+
+            // Specify global extensions and validation layers:
+            val extensionName = stack.UTF8Safe(VK_KHR_SURFACE_EXTENSION_NAME)
+            if (extensionName == null) {
+                throw RuntimeException("Required extens name is null")
+            }
+            val extensions = stack.pointers(extensionName as Buffer)
+            createInfo.ppEnabledExtensionNames(extensions)
+
+            // Create the vulkan instance:
+            val pInstance = stack.mallocPointer(1)
+            val err = vkCreateInstance(createInfo, null, pInstance)
+            if (err != VK_SUCCESS) {
+                throw RuntimeException("Failed to create Vulkan instance: error code $err")
+            }
+            println("Instance information:\n" + createInfo)
+
+            // Return the created instance:
+            vk_instance = VkInstance(pInstance.get(0), createInfo)
+        }
+    }
+
+    // Cleans up and destroys the vulkan instance:
+    fun cleanup() {
+        vk_instance?.let { vkDestroyInstance(it, null) }
+        println("Vulkan instance destroyed successfully.")
+    }
+
+    // Print available extensions:
+    fun check_extensions() {
+        // Check for supported extensions:
+        val supportedExtensions = vk_list_supported_extensions()
+        println("Supported Vulkan Extensions:")
+        supportedExtensions.forEach { println("\t- " + it) }
+    }
+
+    // function for listing all the supported vulkan extensions:
+    fun vk_list_supported_extensions(): List<String> {
+        MemoryStack.stackPush().use { stack ->
+            // Allocate an int buffer to hold the extension count:
+            val ip = stack.mallocInt(1)
+
+            // First call to get the number of extensions:
+            var err = vkEnumerateInstanceExtensionProperties(null as ByteBuffer?, ip, null)
+            if (err != VK_SUCCESS) {
+                throw RuntimeException(
+                        "Failed to get the number instance extensions: err code\n $err"
+                )
+            }
+
+            // Allocate a buffer to hold the extension properties:
+            val extensions = VkExtensionProperties.malloc(ip.get(0), stack)
+
+            // Second call to get the extension properties:
+            err = vkEnumerateInstanceExtensionProperties(null as ByteBuffer?, ip, extensions)
+            if (err != VK_SUCCESS) {
+                throw RuntimeException(
+                        "Failed to enumeratea Vulkan instance extensions: err code\n $err"
+                )
+            }
+
+            // Extract the extension names and return them as a list:
+            return extensions.map { it.extensionNameString() }
+        }
+    }
+}
